5b9a3e4cd5fb0b873e457f69d1eee1c69ec979f8
theDL <david.c.larsen@gmail.com>
2015-10-29 16:24:45 -0700

    DL - added two methods for screens 9 & 15 in ATM class

--- ATM/src/ATM/ATM.java
+++ ATM/src/ATM/ATM.java
@@ -2,4 +2,15 @@ package ATM;
 
 public class ATM {
 
+	
+	public String tenDollarNoteWarning() {
+		// Screen 9
+		// David Larsen
+		return "Machine can only dispense $10 notes.";
+	}
+	public String transactionComplete() {
+		// Screen 15
+		// David Larsen
+		return "Please take your ATM card.\nThank you.";
+	}
 }
