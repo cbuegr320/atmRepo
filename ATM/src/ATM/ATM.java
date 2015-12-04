b894e9a2a806ea3d4008b5ee8cd9cdcfcb93dd60
awadlow <yeswho12@sbcglobal.net>
2015-12-03 16:46:13 -0800

    Added screen 10 error message

--- ATM/src/ATM/ATM.java
+++ ATM/src/ATM/ATM.java
@@ -17,57 +17,62 @@ public class ATM {
 	static double userBalance = 0;
 	
 	public static void main(String[] args) throws SQLException{
-		getAccountInfo(12345);
-		if(isCorrectPIN(4))
-			System.out.println("SUCCESSSSSSSSSSS!!!!!");
-		else
-			System.out.println("FAILLLLL!");
+		
+		updateBalance(1245, 10);
+		
 	}
 	
-	
-	static public void getAccountInfo(int userPAN) throws SQLException{
-		Connection myConnection = null;
-		PreparedStatement myStatement = null;
-		ResultSet myResultSet = null;
-		try {
-			myConnection = DriverManager.getConnection("jdbc:mysql://localhost","student","student");
-			
-			// Prepare statement
-			// This will select a PAN and find the BALANCE that is associated with
-			// that PAN from the database.
-			myStatement = myConnection.prepareStatement("SELECT PAN, PIN, BALANCE FROM MyATMSchema.MyATMTable WHERE PAN="+ userPAN);
-			
-			// Execute SQL query
-			myResultSet = myStatement.executeQuery();
-			
-			// Process Result Set
-			// "PAN" "PIN" and "BALANCE" are the names of the columns in your database.
-			// we assign the variables "thePAN" "thePIN" and "theBalance" to these database columns.
-			while (myResultSet.next()) {
-				PAN = myResultSet.getInt("PAN");
-				PIN = myResultSet.getInt("PIN");
-				userBalance = myResultSet.getInt("BALANCE");
-				System.out.println("Swag PAN: "+ PAN + " PIN: " + PIN + " Balance: " + userBalance);
-			}
-		} 
-		catch (Exception exc) {
-			exc.printStackTrace();
-		} 
-		finally {
-			close (myStatement, myResultSet);
-		}
-	}	
-	
-	
-	public static boolean isCorrectPIN(int tempPIN){
-		return PIN == tempPIN;
-	}
-
-	public String invalidAccountNumber() {
-		//Screen 4
-		//Cameron Geiger
-		return "Invalid ATM card or account number.";
-	}
+//		getAccountInfo(12345);
+//		if(isCorrectPIN(4))
+//			System.out.println("SUCCESSSSSSSSSSS!!!!!");
+//		else
+//			System.out.println("FAILLLLL!");
+//	}
+//	
+//	
+//	static public void getAccountInfo(int userPAN) throws SQLException{
+//		Connection myConnection = null;
+//		PreparedStatement myStatement = null;
+//		ResultSet myResultSet = null;
+//		try {
+//			myConnection = DriverManager.getConnection("jdbc:mysql://localhost","student","student");
+//			
+//			// Prepare statement
+//			// This will select a PAN and find the BALANCE that is associated with
+//			// that PAN from the database.
+//			myStatement = myConnection.prepareStatement("SELECT PAN, PIN, BALANCE FROM MyATMSchema.MyATMTable WHERE PAN="+ userPAN);
+//			
+//			// Execute SQL query
+//			myResultSet = mySta;tement.executeQuery();
+//			
+//			// Process Result Set
+//			// "PAN" "PIN" and "BALANCE" are the names of the columns in your database.
+//			// we assign the variables "thePAN" "thePIN" and "theBalance" to these database columns.
+//			while (myResultSet.next()) {
+//				PAN = myResultSet.getInt("PAN");
+//				PIN = myResultSet.getInt("PIN");
+//				userBalance = myResultSet.getInt("BALANCE");
+//				System.out.println("Swag PAN: "+ PAN + " PIN: " + PIN + " Balance: " + userBalance);
+//			}
+//		} 
+//		catch (Exception exc) {
+//			exc.printStackTrace();
+//		} 
+//		finally {
+//			close (myStatement, myResultSet);
+//		}
+//	}	
+//	
+//	
+//	public static boolean isCorrectPIN(int tempPIN){
+//		return PIN == tempPIN;
+//	}
+//
+//	public String invalidAccountNumber() {
+//		//Screen 4
+//		//Cameron Geiger
+//		return "Invalid ATM card or account number.";
+//	}
 	
 //	public String insufficientFunds() {
 //		//Screen 8
@@ -96,6 +101,14 @@ public class ATM {
 		
 	}
 	
+	public String atmError(){
+		//Screen 10
+		//Austin Wadlow
+		
+		return "Temporarily unable to process withdrawls. Another transaction?";
+	}
+	
+	
 	public static void updateBalance(int userPAN, int userWithdrawal) throws SQLException{
 		// Screen 11
 		// Manuel Puentes
@@ -113,9 +126,9 @@ public class ATM {
 		
 		try{
 			//1. Get a connection to database
-			accountConnection = DriverManager.getConnection("jdbc:mysql://localhost", "student","student");
-			atmConnection = DriverManager.getConnection("jdbc:mysql://localhost", "student","student");
-			atmGetBalance = DriverManager.getConnection("jdbc:mysql://localhost", "student","student");
+			accountConnection = DriverManager.getConnection("jdbc:mysql://localhost", "student", "student");
+			atmConnection = DriverManager.getConnection("jdbc:mysql://localhost", "student", "student");
+			atmGetBalance = DriverManager.getConnection("jdbc:mysql://localhost", "student", "student");
 			
 			//System.out.println("Database connection successful\n");
 			
@@ -140,14 +153,18 @@ public class ATM {
 									  "SET BALANCE =  BALANCE - " + userWithdrawal + " " +
 									  "WHERE PAN = " + ATMUserPAN);
 			atmStatement.executeUpdate("UPDATE myatmschema.myatmmachine " +
-					  "SET atm_balance = atm_balance -" + userWithdrawal + " " +
+					  "SET balance = balance -" + userWithdrawal + " " +
 					  "WHERE  atm_id = " + AtmId);
 			
-			atmBalanceStatement.executeQuery("");
+			
+			
 		
 			//3. Execute SQL query
 			accountResultSet = accountStatement.executeQuery("SELECT * FROM myatmschema.myatmtable ORDER BY PAN");
 			atmResultSet = atmStatement.executeQuery("SELECT * FROM myatmschema.myatmmachine ORDER BY atm_id");
+			
+			
+			
 			
 		}catch(Exception exc){
 			
@@ -175,6 +192,9 @@ public class ATM {
 	private static void close(Statement accountStatement, ResultSet accountResultSet) throws SQLException{
 		close(null, accountStatement,accountResultSet);
 	}
+	
+	
+	
 	
 	public String printReceipt(){
 		//Screen 14
